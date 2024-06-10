fc35acbd2cc14a746e56d1801fb12237f715135b
actuallylemoncurd <curtis.x.jenkins@gmail.com>
2024-06-10 15:09:44 -0400

    update PIF again, decrease kp, increase ki

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -131,8 +131,8 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpBP = [0.0, 41.0]
       ret.lateralTuning.pid.kiBP = [0.0, 41.0]
       ret.lateralTuning.pid.kf = 0.00006
-      ret.lateralTuning.pid.kpV = [0.28, 0.28]
-      ret.lateralTuning.pid.kiV = [0.24, 0.24]
+      ret.lateralTuning.pid.kpV = [0.25, 0.25]
+      ret.lateralTuning.pid.kiV = [0.30, 0.27]
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     elif candidate == CAR.POLO_MK6:
