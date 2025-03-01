5b9cf1c7b2847ac0bfbd38c2c0402d1b670d117e
dkiiv <curtis.x.jenkins@gmail.com>
2025-03-01 15:43:15 -0500

    VWPQPIDF

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -82,7 +82,7 @@ class CarInterface(CarInterfaceBase):
       #             0mph      30mph        60mph       100mph
       # bpoints = [[0., 15.17013889, 30.34027778, 45.51041667, 60.68055556, 75.85069445, 91.02083334]] * 4
       bpoints = [[0., 30.34027778]] * 4
-      values = [[.22, .22], [.0008, 0.0008], [.03, .03], [0.000024, 0.000024]]
+      values = [[.22, .22], [.0005, 0.0005], [.03, .03], [0.000024, 0.000024]]
       #          kpV         kiV           kdV         kfV
 
       ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP, \
