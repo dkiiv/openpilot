334a898dac0e428a603ec520b93c754ee44dd269
dkiiv <curtis.x.jenkins@gmail.com>
2025-06-16 12:54:51 -0400

    Update interface.py

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -120,7 +120,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kiBP = [0., 27.]
       ret.lateralTuning.pid.kpV = [0., 0.]
       ret.lateralTuning.pid.kiV = [0., 0.]
-      ret.lateralTuning.pid.kf = 0.00008
+      ret.lateralTuning.pid.kf = 0.00003
     else:
       ret.steerActuatorDelay = 0.1
       ret.lateralTuning.pid.kpBP = [0.]
