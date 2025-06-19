6525fdecc7dbfeb7763037c2d81b9927d4e5e0e3
actuallylemoncurd <curtis.x.jenkins@gmail.com>
2025-06-19 18:32:32 -0400

    bug fix lost LED control function * orange LED when EPS is not in control

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -253,7 +253,7 @@ class CarController(CarControllerBase):
         pulse = 1
       else:
         pulse = 0
-      can_sends.append(self.CCS.create_lka_hud_control(self.packer_pt, CANBUS.pt, CS.ldw_stock_values, CC.latActive,
+      can_sends.append(self.CCS.create_lka_hud_control(self.packer_pt, CANBUS.pt, CS.ldw_stock_values, (CC.latActive and CS.LH2_steeringState == 64),
                                                        CS.out.steeringPressed, hud_alert, hud_control, pulse))
 
     if self.frame % self.CCP.ACC_HUD_STEP == 0 and self.CP.openpilotLongitudinalControl:
