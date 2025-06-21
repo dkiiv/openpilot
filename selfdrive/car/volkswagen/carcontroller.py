f267dfa25fa6d363ee671930519377b78201e083
Curtis Jenkins <curtis.x.jenkins@gmail.com>
2025-06-21 02:16:17 -0400

    vw pq; no more constant orange on engage. * TODO: we need to just refactor the whole LED control * orange = off * yellow (orange+green) = driver override * green = engaged + steering * flashing orange = engaged + not actively steering/EPS not engaged

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -252,7 +252,10 @@ class CarController(CarControllerBase):
       if CC.latActive and CS.LH2_steeringState != 64 and self.frame % 2:
         pulse = 0
       else:
-        pulse = 1
+        if CC.latActive and CS.LH2_steeringState == 64:
+          pulse = 0
+        else:
+          pulse = 1
       can_sends.append(self.CCS.create_lka_hud_control(self.packer_pt, CANBUS.pt, CS.ldw_stock_values, (CC.latActive and CS.LH2_steeringState == 64),
                                                        CS.out.steeringPressed, hud_alert, hud_control, pulse))
 
